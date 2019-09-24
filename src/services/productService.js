0857da4769a4a0c9cdf0e85c182035ca757cb7fb
Hellden <r.chapotin@gmail.com>
2019-09-24 22:02:08 +0200

    update findIndex productService

--- src/services/productService.js
+++ src/services/productService.js
@@ -19,10 +19,10 @@ export default {
         localStorage.setItem("vuex-commerce-cart", JSON.stringify(cart));
         resolve(cart);
       } else {
+        //JSON parse transform json string in object, option .products is using for specified parent noeud
         const products = JSON.parse(localStorage.getItem("vuex-commerce-cart"))
-          .product;
+          .products;
         // is same product already in cart?
-        debugger;
         const index = products.findIndex(p => p.id === product.id);
         if (index === -1) {
           product.quantity = 1;
